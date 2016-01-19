0146aeb63e6e02a2f4c3f549f6771cf231eff409
Peter Martin <pwm20@pitt.edu>
2016-01-19 11:06:51 -0500

    added passive aggressive comments and printed to the console

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 public class Test {
-
+	//I don't this lab
+	//Our TA didn't show up
     public static void main(String[] args) {
         System.out.println("Test!");
+		System.out.println("Java is SOOOO fun!");
     }
 
 }
