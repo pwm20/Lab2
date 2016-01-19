a6683d996fe7b8c77c1bcf836727520be29981b6
Peter Martin <pwm20@pitt.edu>
2016-01-19 11:11:21 -0500

    Another minor change

--- Test.java
+++ Test.java
@@ -4,6 +4,12 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test!");
 		System.out.println("Java is SOOOO fun!");
+		System.out.print("How many things did I mess up? Lets count... ");
+		for (int i = 0 ; i < 8; i++){
+			if(i != 7)
+				System.out.print( i + ", ");
+			else
+				System.out.print(i + ".");
     }
 
 }
